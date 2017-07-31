4eb5153a47506e703554809c4b28ddb16b08e9ee
Mikhail Zi <zi.white.drago@gmail.com>
2017-07-31 03:41:31 +0300

    Add shared network for combine services and php farm to frontend proxy

--- php-farm/nginx/bin/clear-dir.sh
+++ php-farm/nginx/bin/clear-dir.sh
@@ -3,8 +3,8 @@
 rm -f /etc/nginx/conf.d/*.conf
 rm -f /etc/consul-templates/php/*.ctmpl
 
-for service in $(wget -O - -q http://consul:8500//v1/catalog/services | jq -r 'keys[] | scan("php[0-9]+-[0-9]+-9000")'); do
-    cat /etc/consul-templates/php-server.ctmpl | sed "s|%%SERVICE%%|$service|g" > /etc/consul-templates/php/$(echo -n $service | sed "s|-9000$||").ctmpl
+for service in $(wget -O - -q http://consul:8500//v1/catalog/services | jq -r 'keys[] | scan("php[0-9]+-[0-9]+")'); do
+    cat /etc/consul-templates/php-server.ctmpl | sed "s|%%SERVICE%%|$service|g" > /etc/consul-templates/php/$(echo -n $service).ctmpl
 done
 
 sv hup consul-template-child
