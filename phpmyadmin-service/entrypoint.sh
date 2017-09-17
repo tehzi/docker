7bc538ae9ffd470155d57c4eb1802dcf85ae7cb3
Mikhail Zi <zi.white.drago@gmail.com>
2017-09-17 23:12:19 +0300

    Added pma user for phpmyadmin

--- phpmyadmin-service/entrypoint.sh
+++ phpmyadmin-service/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-sed -i "s/\${DB_USER}/$DB_USER/g; s/\${DB_PASSWORD}/$DB_PASSWORD/g; s/\${HOST}/$HOST/g" /var/www/html/config.inc.php
+sed -i "
+    s/\${DB_USER}/$DB_USER/g;
+    s/\${DB_PASSWORD}/$DB_PASSWORD/g;
+    s/\${HOST}/$HOST/g;
+    s/\${PMA_USER}/$PMA_USER/g;
+    s/\${PMA_PASSWORD}/$PMA_PASSWORD/g
+" /var/www/html/config.inc.php
